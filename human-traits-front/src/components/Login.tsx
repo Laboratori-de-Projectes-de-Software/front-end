e99a3932c1cd10828d18bc9e7037735fa7490e7c
Tenhgsh <mateutorrens84@gmail.com>
2025-03-07 09:31:11 +0100

    añadido links a los botones de Log in y Sign up para que vayan a la pantalla de Account. Añadido Email como campo para registrarse

--- human-traits-front/src/components/Login.tsx
+++ human-traits-front/src/components/Login.tsx
@@ -1,5 +1,5 @@
 import Footer from "./Footer";
-
+import { Link } from "react-router-dom";
 export default function Login(){
   
     return (
@@ -10,22 +10,28 @@ export default function Login(){
           <div className="auth-box">
             <div className="auth-section">
               <h2>Welcome back!</h2>
-              <label>User:</label>
+              <label>User/email:</label>
               <input type="text" placeholder="Enter your username" />
               <label>Password:</label>
               <input type="password" placeholder="Enter your password" />
-              <button className="button-round button-blue">Log In</button>
+              <Link to="/account">
+                <button className="button-round button-blue">Log In</button>
+              </Link>
             </div>
             <div className="separator">or</div>
             <div className="auth-section">
               <h2>Sign Up</h2>
               <label>User:</label>
               <input type="text" placeholder="Choose a username" />
+              <label>Email:</label>
+              <input type="email" placeholder="Enter your email" />
               <label>Password:</label>
               <input type="password" placeholder="Create a password" />
               <label>Repeat Password:</label>
               <input type="password" placeholder="Confirm your password" />
-              <button className="button-round button-blue">Sign Up</button>
+              <Link to="/account">
+                <button className="button-round button-blue">Sign Up</button>
+              </Link>
             </div>
           </div>
         </div>
