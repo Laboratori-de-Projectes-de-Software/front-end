15638645ff330c4c89930db66698f40629df45e3
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2025-03-15 11:46:35 +0100

    Plana de classifcació acabada - Més altre modificacions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ParticipantsPage from './routes/ParticipantsPage'
+import ClassificationPage from './routes/ClassificationPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/participants" element={<ParticipantsPage />} />
+        <Route path="/classification" element={<ClassificationPage />} />
       </Routes>
     </Router>
   )
---
+++ src/routes/ClassificationPage.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import Navbar from "../components/Navbar"; 
+import AddLeagueBar from "../components/AddLeagueBar"; 
+
+const leagues = ["League 1", "League 2", "League 3"];
+const participants = [
+  { name: "Bot 1", points:20, debates: 10, wins: 6, draws: 2, losses: 2 },
+  { name: "Bot 5", points:15, debates: 8, wins: 4, draws: 3, losses: 1 },
+  { name: "Bot 4", points:11, debates: 7, wins: 3, draws: 2, losses: 2 },
+];
+
+const LeagueTable: React.FC = () => {
+  const [selectedLeague, setSelectedLeague] = useState("League 2");
+
+  return (
+    <div className="min-h-screen">
+         <Navbar />
+      
+      {/* Contingut pagina */}
+      <main className="flex flex-row items-start gap-8">
+       
+        {/* Barra Lateral // mirar dins components */}
+        <AddLeagueBar leagues={leagues} selectedLeague={selectedLeague} onSelectLeague={setSelectedLeague}/>
+
+
+        <div className="flex-1">
+            <h1 className="pt-5 pb-2 text-3xl font-bold text-center mb-4">{selectedLeague}</h1>
+
+            {/* Botons */}
+            <div className="pb-12 flex justify-center mb-4">
+                <button className="bg-(--clas-btn-sel) px-4 py-2 rounded-l">Classification</button>
+                {/*---------->  Falta ficar enllaç a pàgina que pertoca */}
+                <button className="bg-(--secondary) px-4 py-2 rounded-r">Debate clashes</button>
+            </div>
+
+            
+            {/* Taula classificació */}
+            <div className="pr-10 pl-10 overflow-x-auto">
+                <table className="w-full border border-(--table-border)">
+                <thead className="bg-(--table-index)  text-white">
+                    <tr className="border border-(--table-border)">
+                    <th className="p-2 border-r border-(--table-border)">Bot</th>
+                    <th className="p-2 border-r border-(--table-border)">Points</th>
+                    <th className="p-2 border-r border-(--table-border)">Debates</th>
+                    <th className="p-2 border-r border-(--table-border)">Wins</th>
+                    <th className="p-2 border-r border-(--table-border)">Draw</th>
+                    <th className="p-2 border-r border-(--table-border)">Losses</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {participants.map((bot) => (
+                    <tr key={bot.name} className="bg-(--secondary) text-center">
+                        <td className="p-2 border-r border-(--table-border)">{bot.name}</td>
+                        <td className="p-2 border-r border-(--table-border)">{bot.points}</td>
+                        <td className="p-2 border-r border-(--table-border)">{bot.debates}</td>
+                        <td className="p-2 border-r border-(--table-border)">{bot.wins}</td>
+                        <td className="p-2 border-r border-(--table-border)">{bot.draws}</td>
+                        <td className="p-2 border-r border-(--table-border)">{bot.losses}</td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default LeagueTable;
