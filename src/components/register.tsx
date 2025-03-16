8757b1d603eb83141afeffaaefcb30287b0c5660
mponsetim <matias.ponseti1@estudiant.uib.cat>
2025-03-16 12:10:45 +0100

    feat: :sparkles: Formulario de registro de una nueva cuenta

---
+++ src/components/register.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Input from "./Input.tsx";
+import type { InputConfig } from "@interfaces/Input-config";
+import "@styles/Login.style.css";
+import axios from "axios";
+
+const Login: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const apiUrl = "www.ejemplo.com"; // TODO: Cambiar la url de la api
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    axios.post(apiUrl, { name, email, password }).then((response) => {
+      alert(response);
+    });
+  };
+
+  const nameInputConfig: InputConfig = {
+    id: "register-name",
+    label: "Nombre",
+    state: setName,
+  };
+
+  const emailInputConfig: InputConfig = {
+    id: "register-email",
+    label: "Email",
+    state: setEmail,
+  };
+
+  const passwordInputConfig: InputConfig = {
+    id: "register-password",
+    label: "Contraseña",
+    state: setPassword,
+  };
+
+  return (
+    <form className="register-form" onSubmit={handleSubmit}>
+      <h1>Registarse</h1>
+      <h3>Regístrate para poder acceder a la web</h3>
+      <Input config={nameInputConfig} />
+      <Input config={emailInputConfig} />
+      <Input config={passwordInputConfig} />
+      <button className="register-form__submit" type="submit">
+        Crear cuenta
+      </button>
+      <p className="register-form__sign-in-reminder">
+        Ya tienes una cuenta?{" "}
+        <span className="register-form__sign-in-link">Inicia sesión</span>
+      </p>
+    </form>
+  );
+};
+
+export default Login;
