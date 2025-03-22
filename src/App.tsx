209adbe5859c42abf9a5ce6aff8e69a08c050bc4
Sergio Gamundi Barrientos <sergio.gb99@hotmail.com>
2025-03-22 09:12:12 +0100

    feat: :sparkles: Add Hero component with styles and integrate into Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./modules/shared/header/Header";
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Home />
+      </main>
     </>
   );
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+import Hero from "../modules/hero/Hero.tsx";
+
+const Home: React.FC = () => {
+  return <Hero />;
+};
+
+export default Home;
