45e80265c14fe8e3c191de36a459460ed98ab1c4
mponsetim <matias.ponseti1@estudiant.uib.cat>
2025-03-29 20:49:32 +0100

    feat: :sparkles: add TextInput component with styling and error handling

---
+++ src/modules/shared/input/text-input/text-input.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import './text-input.scss';
+
+type Props = {
+  value: string | undefined | null;
+  setValue: (value: string) => void;
+  text?: string;
+}
+
+const TextInput: FC<Props> = ({value, setValue, text}) => {
+
+  return (
+    <div className="form-field__container">
+        <input
+          required
+          id='text-input'
+          className="form-field__input"
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+          value={value || ''}
+          />
+          {text ? 
+            <label htmlFor='text-input' className="form-field__label">
+              {text}
+            </label>
+          : 
+            <></>
+          }
+      </div>
+  )
+};
+
+export default TextInput;
