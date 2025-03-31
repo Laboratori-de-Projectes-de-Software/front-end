069b398a86dc8989f541e4ce87720f3cf74152a3
Alejandro Soto <106865529+AlejandroSotoGit@users.noreply.github.com>
2025-03-31 19:04:34 +0200

    Profile page with placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import PerfilPage from "./pages/PerfilPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export function App() {
 
   return (
-    <>
-      
-    </>
+      <Router>
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/profile" element={<PerfilPage />} /> {/* Ruta para el perfil */}
+          </Routes>
+      </Router>
   )
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage: React.FC = () => {
+    return (
+        <div className="container mt-4 text-center">
+            <h1 className="fw-bold">Homepage</h1>
+            <p>Bienvenido a Cyber Arena.</p>
+        </div>
+    );
+}
+
+export default HomePage;
