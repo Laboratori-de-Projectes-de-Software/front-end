0101988838106f5ddd19e411e66f0de025fb10f1
Cristobal <calbertimartinez@gmail.com>
2025-04-02 19:46:48 +0200

    bots
    todo la interfaz de bots terminada

--- src/pages/Bots.tsx
+++ src/pages/Bots.tsx
@@ -6,6 +6,8 @@ import R3 from "../assets/img/R3.png";
 import R4 from "../assets/img/R4.png";
 import R5 from "../assets/img/R5.png";
 import ADDICON from "../assets/img/addIcon.png"; 
+import EDIT from "../assets/img/Editar.png";
+import DELETE from "../assets/img/Eliminar.png";
 
 export function Bots() {
     return (
@@ -15,12 +17,28 @@ export function Bots() {
                 <div className={style.bots}>
                     <h2>My Bots</h2>
                     <ul>
-                        <li style = {{backgroundImage: `url(${R1})`}}>Bot 1</li>
-                        <li style = {{backgroundImage: `url(${R2})`}}>Bot 2</li>
-                        <li style = {{backgroundImage: `url(${R3})`}}>Bot 3</li>
-                        <li style = {{backgroundImage: `url(${R4})`}}>Bot 4</li>
-                        <li style = {{backgroundImage: `url(${R5})`}}>Bot 5</li>
-                        <li style = {{backgroundImage: `url(${ADDICON})`}}>ADD NEW</li>
+                        <div className={style.botsHeader} style = {{backgroundImage: `url(${R1})`}}>
+                            <li>Bot 1</li>
+                            <button className = {style.edit} style = {{backgroundImage: `url(${EDIT})`}}></button><button className = {style.delete} style = {{backgroundImage: `url(${DELETE})`}}></button>
+                        </div>
+                        <div className={style.botsHeader} style = {{backgroundImage: `url(${R1})`}}>
+                            <li>Bot 2</li>
+                            <button className = {style.edit} style = {{backgroundImage: `url(${EDIT})`}}></button><button className = {style.delete} style = {{backgroundImage: `url(${DELETE})`}}></button>
+                        </div>
+                        <div className={style.botsHeader} style = {{backgroundImage: `url(${R1})`}}>
+                            <li>Bot 3</li>
+                            <button className = {style.edit} style = {{backgroundImage: `url(${EDIT})`}}></button><button className = {style.delete} style = {{backgroundImage: `url(${DELETE})`}}></button>
+                        </div>
+                        <div className={style.botsHeader} style = {{backgroundImage: `url(${R1})`}}>
+                            <li>Bot 4</li>
+                            <button className = {style.edit} style = {{backgroundImage: `url(${EDIT})`}}></button><button className = {style.delete} style = {{backgroundImage: `url(${DELETE})`}}></button>
+                        </div>
+                        <div className={style.botsHeader} style = {{backgroundImage: `url(${R1})`}}>
+                            <li>Bot 5</li>
+                            <button className = {style.edit} style = {{backgroundImage: `url(${EDIT})`}}></button><button className = {style.delete} style = {{backgroundImage: `url(${DELETE})`}}></button>
+                        </div>
+                        <div className={style.botsHeader} style = {{backgroundImage: `url(${ADDICON})`}}>
+                        </div>
                     </ul>
                 </div>
             </div>
