3370367e5d5d02cf8448920badd7bcca3e9d386c
Cristobal <calbertimartinez@gmail.com>
2025-04-02 18:20:20 +0200

    a

--- src/pages/Bots.tsx
+++ src/pages/Bots.tsx
@@ -1,8 +1,29 @@
+import NavBar from "../components/NavBar";
+import style from "./Bots.module.css";
+import R1 from "../assets/img/R1.png";
+import R2 from "../assets/img/R2.png";
+import R3 from "../assets/img/R3.png";
+import R4 from "../assets/img/R4.png";
+import R5 from "../assets/img/R5.png";
+import ADDICON from "../assets/img/addIcon.png"; 
+
 export function Bots() {
     return (
         <div>
-            <h1>Bots</h1>
-            <p>Welcome to the Bots page!</p>
+            <NavBar/>
+            <div className = {style.background}>
+                <div className={style.bots}>
+                    <h2>My Bots</h2>
+                    <ul>
+                        <li style = {{backgroundImage: `url(${R1})`}}>Bot 1</li>
+                        <li style = {{backgroundImage: `url(${R2})`}}>Bot 2</li>
+                        <li style = {{backgroundImage: `url(${R3})`}}>Bot 3</li>
+                        <li style = {{backgroundImage: `url(${R4})`}}>Bot 4</li>
+                        <li style = {{backgroundImage: `url(${R5})`}}>Bot 5</li>
+                        <li style = {{backgroundImage: `url(${ADDICON})`}}>ADD NEW</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
