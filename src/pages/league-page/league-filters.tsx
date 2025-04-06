fd8ef270c456a638449c17579325efec7c6e10e6
Sergio Gamundi Barrientos <103444875+Gamundi1@users.noreply.github.com>
2025-04-06 18:37:46 +0200

    Update league-filters.tsx

--- src/pages/league-page/league-filters.tsx
+++ src/pages/league-page/league-filters.tsx
@@ -9,6 +9,7 @@ type Props = {
 };
 
 const LeagueFilters: FC<Props> = ({filters, setFilters}) => {
+    
   return (
     <div className="league-filters-container">
         <h2 className="league-filters-title">Filtros</h2>
