5e742ff921595d65edae43eb6330b87a3da06756
Jaume <jribasgaya@gmail.com>
2025-04-07 20:23:58 +0200

    create some attributes to view

--- src/DTOClasses/BotDTO.ts
+++ src/DTOClasses/BotDTO.ts
@@ -1,3 +1,9 @@
 export default interface BotDTO {
     botId: number
+    // only for example in ParticipantsPage.tsx
+    name: string
+    team: string
+    wins: number
+    draws: number
+    losses: number
 };
