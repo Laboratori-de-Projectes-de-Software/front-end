7bd679816376b6b0dc2cf9948b51f709af681acb
Sergio Gamundi Barrientos <sergio.gb99@hotmail.com>
2025-04-08 17:34:40 +0200

    añade algunos estilos y arregla cosas

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from "./pages/home/Home.tsx";
 import LeaguesPage from "./pages/league-page/leagues-page.tsx";
 import ModalManager from "./modules/modalManager/ModalManager.tsx";
 import { ModalProvider } from "./modules/modalManager/ModalProvider.tsx";
+import NotFound from "./pages/NotFound/NotFound.tsx";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <Route element={<PrivateRoute />}>
                 <Route path="/league" element={<LeaguesPage />} />
               </Route>
+              <Route path="*" element={<NotFound />} />
             </Routes>
             <ModalManager />
           </ModalProvider>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,26 +1,52 @@
-import Modal from "../../modules/shared/modal/Modal";
-import Login from "../../modules/forms/login/Login.tsx";
-import { useState } from "react";
 import "./Home.scss";
+import StarField from "../../modules/home/stars/stars.tsx";
+import { useState, useEffect } from "react";
+import { useModal } from "../../modules/modalManager/ModalProvider.tsx";
+import { useAuth } from "../../auth/AuthProvider.tsx";
+import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = () => {
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [blurAmount, setBlurAmount] = useState(0)
+  const auth = useAuth();
+  const { openModal }  = useModal();
+  const navigate = useNavigate();
 
-  const openLoginModal = () => setIsLoginModalOpen(true);
-  const closeLoginModal = () => setIsLoginModalOpen(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const maxBlur = 8
+      const triggerHeight = window.innerHeight * 1.2
+      const newBlurAmount = Math.min(maxBlur, (window.scrollY / triggerHeight) * maxBlur)
+
+      setBlurAmount(newBlurAmount)
+    }
+
+    window.addEventListener("scroll", handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  const redirectToLeague = () => {
+    if (auth?.isUserLoggedIn()) {
+      navigate("/league")
+    } else {
+      openModal("login")
+    }
+  }
+
   return (
     <section className="home">
-      <img src="background.jpg" alt="" className="home__background" />
+      <StarField blurAmount={blurAmount} />
       <h1 className="home__title">
-        Accede para poder competir contra otros bots
+        Accede para poder unirte a una liga
       </h1>
-      <button className="home__login-button" onClick={openLoginModal}>
-        <span>Entrar</span>
-        <img src="arrow-right.svg" height={12}></img>
+      <button className="home__login-button" onClick={redirectToLeague}>
+        <span className="top-key"></span>
+        <span className="text">ENTRAR</span>
+        <span className="bottom-key-1"></span>
+        <span className="bottom-key-2"></span>
       </button>
-      <Modal isOpen={isLoginModalOpen} onClose={closeLoginModal}>
-        <Login />
-      </Modal>
     </section>
   );
 };
