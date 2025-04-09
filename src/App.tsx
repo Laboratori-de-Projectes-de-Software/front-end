4e14d2edae18ddc521703c88b9c49137b8324d9a
AlexDev-008 <alex.rodriguez1@estudiant.uib.cat>
2025-04-09 17:58:40 +0200

    Created log in and centralized api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
 import MenuComponent from "./components/MenuComponent.tsx";
 import {Col, Row} from "react-bootstrap";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import MisBots from "./pages/MisBots.tsx";
 import MisLigas from "./pages/MisLigas.tsx";
 import {useState} from "react";
 import PerfilPage from "./pages/PerfilPage.tsx";
+import LogIn from "./pages/LogIn.tsx";
 
 export function App() {
+    const location = useLocation();
   const [menuExpanded, setMenuExpanded] = useState(true);
+  const isLoginPage = location.pathname === "/login";
 
   return (
       <>
-        <Row className="custom-primary p-3 gx-0" style={{ height: '100vh'}}>
-          <Col xs={menuExpanded ? 2 : 1} style={{transition: "width  .3s ease-in-out"}}>
-            <MenuComponent menuExpanded={menuExpanded} handleMenuExpanded={setMenuExpanded}></MenuComponent>
-          </Col>
-          <Col xs={menuExpanded ? 10 : 11} className="custom-secondary rounded-3 p-5" style={{transition: "width  .3s ease-in-out"}}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/mis-bots" element={<MisBots />} />
-              <Route path="/mis-ligas" element={<MisLigas />} />
-              <Route path="/perfil/:id" element={<PerfilPage />} />
-            </Routes>
-          </Col>
-        </Row>
+        {isLoginPage ?
+            <LogIn />
+            :
+            <Row className="custom-primary p-3 gx-0" style={{ height: '100vh'}}>
+              <Col xs={menuExpanded ? 2 : 1} style={{transition: "width  .3s ease-in-out"}}>
+                <MenuComponent menuExpanded={menuExpanded} handleMenuExpanded={setMenuExpanded}></MenuComponent>
+              </Col>
+              <Col xs={menuExpanded ? 10 : 11} className="custom-secondary rounded-3 p-5" style={{transition: "width  .3s ease-in-out"}}>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/mis-bots" element={<MisBots />} />
+                  <Route path="/mis-ligas" element={<MisLigas />} />
+                  <Route path="/perfil/:id" element={<PerfilPage />} />
+                </Routes>
+              </Col>
+            </Row>
+        }
       </>
   )
 }
