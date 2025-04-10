11f659d08b233b5e8639a660f28ce6a1c84557d6
Sergio Gamundi Barrientos <sergio.gb99@hotmail.com>
2025-04-10 21:15:48 +0200

    e

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import { store } from './features/shared/store.ts'
 import './index.css'
 import App from './App.tsx'
 
+if (import.meta.env.MODE === 'development') {
+  const { worker } = await import('./mocks/browser.ts')
+  worker.start()
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
