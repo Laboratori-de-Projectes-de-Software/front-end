61777233b7e2d7de5742ba5ec5c294f63b2384f7
Golivar <biel@viratge.com>
2025-04-10 12:17:39 +0200

    Global API conection defined and set to a ConBack instance

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import {ConAPI} from './components/ConAPI'
+import {ConBack} from './components/ConBack'
 import App from './App'
 
+declare global {
+    interface Window {
+        APIConection: ConAPI;  // or a more specific type
+    }
+}
+
+// Initialize it
+export function initGlobals(){
+  window.APIConection = new ConBack();
+}
+
+initGlobals();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
