1868cafffcd3f041bb435fd88c9a5ca972f38cda
Andres95123 <andrescpcoplldenrabassa@gmail.com>
2025-04-11 14:15:07 +0200

    History Of Bots

--- src/components/BotList.tsx
+++ src/components/BotList.tsx
@@ -1,75 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BotDetails from "./BotDetails";
 
 interface Bot {
-  id: number;
+  botId: number;
+  description: string;
   name: string;
-  emotion: string;
-  image: string;
-  user: string;
+  ndraws: number;
+  nloses: number;
+  nwins: number;
+  urlImagen: string;
 }
 
-const initialBots: Bot[] = [
-  {
-    id: 1,
-    name: "BOT 1",
-    emotion: "TRISTEZA",
-    image: "/bots/tristeza.png",
-    user: "/users/user1.jpg",
-  },
-  {
-    id: 2,
-    name: "BOT 2",
-    emotion: "ASCO",
-    image: "images/asco.jpg",
-    user: "/users/user2.jpg",
-  },
-  {
-    id: 3,
-    name: "BOT 3",
-    emotion: "ALEGRÍA",
-    image: "/bots/alegria.png",
-    user: "/users/user3.jpg",
-  },
-  {
-    id: 4,
-    name: "BOT 4",
-    emotion: "IRA",
-    image: "/bots/ira.png",
-    user: "/users/user4.jpg",
-  },
-  {
-    id: 5,
-    name: "BOT 5",
-    emotion: "ENVIDIA",
-    image: "images/envy.webp",
-    user: "/users/user5.jpg",
-  },
-  {
-    id: 6,
-    name: "BOT 6",
-    emotion: "MIEDO",
-    image: "/bots/miedo.png",
-    user: "/users/user6.jpg",
-  },
-  {
-    id: 7,
-    name: "BOT 7",
-    emotion: "VERGÜENZA",
-    image: "/bots/verguenza.png",
-    user: "/users/user7.jpg",
-  },
-  {
-    id: 8,
-    name: "BOT 8",
-    emotion: "ANSIEDAD",
-    image: "images/ansiedad.jpeg",
-    user: "/users/user8.jpg",
-  },
-];
-
 export const BotList = () => {
-  const [bots, setBots] = useState<Bot[]>(initialBots);
+  const [bots, setBots] = useState<Bot[]>([]);
+
+  async function initBots(): Promise<Bot[]> {
+    // Fetch to api
+    try {
+      const response = await fetch("http://localhost:8080/bot");
+      if (!response.ok) {
+        console.error("API response error:", response.status);
+        return []; // Return empty array on error
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch bots:", error);
+      return []; // Return empty array on error
+    }
+  }
+
+  useEffect(() => {
+    const fetchBots = async () => {
+      const initialBots = await initBots();
+      setBots(initialBots);
+    };
+    fetchBots();
+  }, []);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [clickedBot, setClickedBot] = useState<number | null>(null);
 
@@ -93,7 +60,7 @@ export const BotList = () => {
   }
 
   if (clickedBot) {
-    const selectedBot = bots.find((bot) => bot.id === clickedBot);
+    const selectedBot = bots.find((bot) => bot.botId === clickedBot);
     if (!selectedBot) return null;
 
     // Completamos datos que no estén presentes en BotList con valores por defecto
@@ -141,9 +108,9 @@ export const BotList = () => {
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           {filteredBots.map((bot) => (
             <BotCard
-              key={bot.id}
+              key={bot.botId}
               bot={bot}
-              onClick={() => handleBotClick(bot.id)}
+              onClick={() => handleBotClick(bot.botId)}
             />
           ))}
         </div>
@@ -167,23 +134,23 @@ const BotCard: React.FC<BotCardProps> = ({ bot, onClick }) => {
     >
       <div className="relative rounded-t-xl overflow-hidden">
         <img
-          src={bot.image}
+          src={bot.urlImagen}
           alt={bot.name}
           className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out"
         />
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black/50" />
         <span className="absolute top-2 right-2 bg-black/70 text-white text-xs px-3 py-1.5 rounded-full font-medium border border-gray-700">
-          {bot.emotion}
+          {/* {TODO} */}
         </span>
       </div>
 
       <div className="p-4">
         <div className="flex items-center gap-2">
-          <img
+          {/* <img
             src={bot.user}
             alt="user"
             className="w-8 h-8 rounded-full border-2 border-white shadow-lg"
-          />
+          /> */}
           <div>
             <h3 className="text-lg font-semibold text-white group-hover:text-blue-600">
               {bot.name}
@@ -210,7 +177,7 @@ const BotCard: React.FC<BotCardProps> = ({ bot, onClick }) => {
                 d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
               />
             </svg>
-            <span className="text-xs text-white">12</span>
+            <span className="text-xs text-white">{bot.nwins}</span>
           </div>
           <div className="flex items-center gap-1">
             <svg
@@ -227,7 +194,7 @@ const BotCard: React.FC<BotCardProps> = ({ bot, onClick }) => {
                 d="M3.75 9h16.5m-16.5 6.75h16.5"
               />
             </svg>
-            <span className="text-xs text-white">3</span>
+            <span className="text-xs text-white">{bot.ndraws}</span>
           </div>
           <div className="flex items-center gap-1">
             <svg
@@ -244,7 +211,7 @@ const BotCard: React.FC<BotCardProps> = ({ bot, onClick }) => {
                 d="M6 18L18 6M6 6l12 12"
               />
             </svg>
-            <span className="text-xs text-white">5</span>
+            <span className="text-xs text-white">{bot.nloses}</span>
           </div>
         </div>
         <button className="py-1.5 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-gray-700 bg-black/70 text-white hover:bg-black/90">
