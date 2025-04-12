9ef00aabda7811e1fe32fded68a565cba02d5bfd
AlexDev-008 <alex.rodriguez1@estudiant.uib.cat>
2025-04-12 11:24:59 +0200

    Added league state filter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import {faCircle, faUser} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import imgPlaceholder from "../assets/img/ligabanner.jpg";
 import {NavLink} from "react-router-dom";
+import Form from 'react-bootstrap/Form';
 
 interface Ligas {
     leagueId: number
@@ -19,6 +20,7 @@ interface Ligas {
 
 const TodasLasLigas = () => {
     const [ligas, setLigas] = useState<Ligas[]>([]);
+    const [filter, setFilter] = useState<string>("");
 
     const stateColor = {
         "ABIERTA": "#198754",
@@ -36,48 +38,60 @@ const TodasLasLigas = () => {
     }, []);
 
     return (
-        <div className="p-6">
+        <div>
             <h1 className="text-3xl font-bold mb-6">HOME</h1>
-                {
-                    ligas.length === 0 ?
-                        <p className="fs-4 mt-4">No hay ligas disponibles actualmente</p>
-                        :
-                        <>
-                        <Row className="d-flex align-items-center justify-content-between rounded-4">
-                            {ligas.map((liga) => (
-                                <Col key={liga.leagueId} md={5} lg={3} className="custom-primary rounded-3 text-light p-0 mt-5">
-                                    <img src={imgPlaceholder} alt="Imagen liga"
-                                         className="object-fit- w-100 overflow-hidden rounded-top-4"
-                                         style={{height: "175px"}}/>
-                                    <div className="p-3">
-                                        <Row className="d-flex align-items-center mb-2 mt-3">
-                                            <div>
-                                                <FontAwesomeIcon icon={faCircle}
-                                                                 color={stateColor[liga.state] || "#000"} size={"sm"}
-                                                                 style={{maxWidth: "15px"}}/>
-                                                <p className="d-inline ms-2"
-                                                   style={{color: stateColor[liga.state]}}>{liga.state}</p>
-                                            </div>
-                                        </Row>
-                                        <h1 className="fs-3">{liga.name}</h1>
-                                        <Row className="d-flex align-items-center mb-2">
-                                            <div>
-                                                <FontAwesomeIcon icon={faUser} size={"lg"} style={{maxWidth: "15px"}}/>
-                                                <p className="d-inline ms-2">{liga.user}</p>
-                                            </div>
-                                        </Row>
-                                        <p>{liga.bots.length} {liga.bots.length === 1 ? "participante" : "participantes"}</p>
-                                        <NavLink to={`/league/${liga.leagueId}`} className="text-decoration-none">
-                                            <Button className="custom-action w-100">
-                                                Ver Liga
-                                            </Button>
-                                        </NavLink>
-                                    </div>
-                                </Col>
-                            ))}
-                        </Row>
-                        </>
-                }
+            <Row className="d-flex justify-content-end">
+                <Form.Select aria-label="Filtrar ligas"
+                             className="w-25 custom-primary text-white custom-select-white"
+                             onChange={(e) => setFilter(e.target.value)}
+                >
+                    <option value="">Todas</option>
+                    <option value="ABIERTA">Abiertas</option>
+                    <option value="EN CURSO">En curso</option>
+                    <option value="FINALIZADA">Finalizadas</option>
+                </Form.Select>
+            </Row>
+            {
+                ligas.length === 0 ?
+                    <p className="fs-4 mt-4">No hay ligas disponibles actualmente</p>
+                    :
+                    <>
+                    <Row className="d-flex align-items-center justify-content-between rounded-4">
+                        {ligas.map((liga) => (
+                            (liga.state.toUpperCase() === filter || filter === "") &&
+                            <Col key={liga.leagueId} md={5} lg={3} className="custom-primary rounded-3 text-light p-0 mt-5 mx-2">
+                                <img src={imgPlaceholder} alt="Imagen liga"
+                                     className="object-fit- w-100 overflow-hidden rounded-top-4"
+                                     style={{height: "175px"}}/>
+                                <div className="p-3">
+                                    <Row className="d-flex align-items-center mb-2 mt-3">
+                                        <div>
+                                            <FontAwesomeIcon icon={faCircle}
+                                                             color={stateColor[liga.state] || "#000"} size={"sm"}
+                                                             style={{maxWidth: "15px"}}/>
+                                            <p className="d-inline ms-2"
+                                               style={{color: stateColor[liga.state]}}>{liga.state}</p>
+                                        </div>
+                                    </Row>
+                                    <h1 className="fs-3">{liga.name}</h1>
+                                    <Row className="d-flex align-items-center mb-2">
+                                        <div>
+                                            <FontAwesomeIcon icon={faUser} size={"lg"} style={{maxWidth: "15px"}}/>
+                                            <p className="d-inline ms-2">{liga.user}</p>
+                                        </div>
+                                    </Row>
+                                    <p>{liga.bots.length} {liga.bots.length === 1 ? "participante" : "participantes"}</p>
+                                    <NavLink to={`/league/${liga.leagueId}`} className="text-decoration-none">
+                                        <Button className="custom-action w-100">
+                                            Ver Liga
+                                        </Button>
+                                    </NavLink>
+                                </div>
+                            </Col>
+                        ))}
+                    </Row>
+                    </>
+            }
         </div>
     );
 };
