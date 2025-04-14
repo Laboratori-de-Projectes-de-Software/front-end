1c544b72c38991aaa4d3effa283d470bebb2ba99
Jaume <jribasgaya@gmail.com>
2025-04-14 18:20:03 +0200

    delete team prop

--- src/components/ParticipantCard.tsx
+++ src/components/ParticipantCard.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 
 interface ParticipantCardProps {
   name: string;
-  team: string;
   wins: number;
   draws: number;
   losses: number;
 }
 
-const ParticipantCard: React.FC<ParticipantCardProps> = ({ name, team, wins, draws, losses }) => {
+const ParticipantCard: React.FC<ParticipantCardProps> = ({ name, wins, draws, losses }) => {
   return (
     <div className="flex justify-between content-center bg-(--secondary) p-4 rounded-lg shadow-md w-64">
-      <div className='flex flex-col justify-center'>
-        <h2 className="text-2xl font-bold">{name}</h2>
-        <p className="text-sm text-(--bot-card-color)">Equipo: {team}</p>
-      </div>
+      <h2 className="text-2xl font-bold">{name}</h2>
       <div className="flex flex-col items-end">
         <span className="text-(--color-green)">{wins}V</span>
         <span className="text-(--color-yellow)">{draws}E</span>
