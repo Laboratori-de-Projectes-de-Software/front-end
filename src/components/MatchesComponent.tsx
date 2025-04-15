6732e998b96ea68d12065cc129d6279fdae11604
CharlieUIB <carlos.galvez1@estudiant.uib.cat>
2025-04-16 00:21:59 +0200

    made the macthes component redirect to the matchPage

--- src/components/MatchesComponent.tsx
+++ src/components/MatchesComponent.tsx
@@ -1,15 +1,18 @@
 import { MatchDTO } from "@DTOClasses/MatchDTO";
+import { useNavigate } from "react-router-dom";
 
 interface Params {
     matches: MatchDTO[]
 }
 
 export const MatchesComponent: React.FC<Params> = ({matches}) => {
+    const navigate = useNavigate();
     return(<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {matches.length > 0 && matches.map((match, index) => (
           <div
             key={index}
             className="bg-black text-white text-lg p-4 rounded-lg text-center h-28"
+            onClick={() => navigate(`/match/${match.id}`)}
           >
             <p className="font-bold text-gray-400">{match.fighters[0]} vs {match.fighters[1]}</p>
             {index < 7 ? (
