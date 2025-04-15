29a7e1583085bded9b84e1ae784c0ae82b67f843
CharlieUIB <carlos.galvez1@estudiant.uib.cat>
2025-04-15 23:34:40 +0200

    changed routes and reorder imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import WelcomePage from '@routes/WelcomePage'
 import LoginPage from '@routes/LoginPage'
 import RegisterPage from '@routes/RegisterPage'
 import ParticipantsPage from '@routes/ParticipantsPage'
-import ClassificationPage from '@routes/ClassificationPage'
-import ConfrontationsPage from '@routes/ConfrontationsPage'
 import Navbar from '@components/Navbar'
 import AddLeaguePage from '@routes/AddLeaguePage'
 import AddParticipantPage from '@routes/AddParticipantPage'
 import Match from '@routes/MatchPage'
 import LogOutPage from '@routes/LogOutPage'
+import LeaguesPage from '@routes/LeaguesPage'
 
 function App() {
   return (
@@ -24,7 +23,7 @@ function App() {
           <Route path="/bots" element={<ParticipantsPage />} />
           {/* <Route path="/leagues/:leagueId/participants" element={<ParticipantsPage />} />
           <Route path="/leagues/:leagueId/classification" element={<ClassificationPage />} /> */}
-          <Route path="/leagues" element={<ConfrontationsPage />} />
+          <Route path="/leagues" element={<LeaguesPage />} />
           <Route path="/add-league" element={<AddLeaguePage />} />
           <Route path="/add-bot" element={<AddParticipantPage />} />
           <Route path="/match/:matchId" element={<Match />} />
