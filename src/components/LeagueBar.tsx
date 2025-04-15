0626f30e34e9c168a3fd5197c8ee6cbe5bb2ea3f
CharlieUIB <carlos.galvez1@estudiant.uib.cat>
2025-04-15 20:23:53 +0200

    fix style

--- src/components/LeagueBar.tsx
+++ src/components/LeagueBar.tsx
@@ -14,20 +14,21 @@ const LeagueBar: React.FC<LeagueBarProps> = ({ leagues, selectedLeagueId, onSele
   const navigate = useNavigate();
 
   return (
-    <aside className="w-1/4 bg-black p-4 text-white min-h-screen">
-      <WhiteButton className="w-full text-lg" onClick={() => navigate("/add-league")}>+ Add League</WhiteButton>
-      
-      {leagues.map((league) => (
-        <button
-          key={league.leagueId}
-          className={`w-full py-2 mb-2 rounded-lg font-medium text-md ${
-            selectedLeagueId === league.leagueId ? "bg-(--lliga-btn-sel)" : "bg-(--lliga-btn)"
-          }`}
-          onClick={() => onSelectLeague(league)}
-      >
-        {league.name}
-      </button>
-      ))}
+    <aside className="w-1/5 bg-black p-4 text-white min-h-screen">
+      <WhiteButton className="w-full text-lg my-3 mb-6" onClick={() => navigate("/add-league")}>+ Add League</WhiteButton>
+      <div className="flex flex-col gap-y-1">
+        {leagues.map((league) => (
+          <button
+            key={league.leagueId}
+            className={`w-full py-2 mb-2 rounded-lg font-medium text-md ${
+              selectedLeagueId === league.leagueId ? "bg-(--lliga-btn-sel)" : "bg-(--lliga-btn)"
+            }`}
+            onClick={() => onSelectLeague(league)}
+        >
+          {league.name}
+        </button>
+        ))}
+      </div>
     </aside>
   );
 };
