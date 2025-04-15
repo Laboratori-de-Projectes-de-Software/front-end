3141854d01ac02bdc6f0c7e20bc7db79b15ee4b6
CharlieUIB <carlos.galvez1@estudiant.uib.cat>
2025-04-15 16:54:44 +0200

    fix route name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,10 @@ const Navbar: React.FC = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-(--navbar) h-20">
       <div className="flex space-x-8 content-center">
-        <a href="/"><span className="text-xl font-bold">Web name</span></a>
+        <a href="/"><span className="text-xl font-bold">Bot Debate League</span></a>
         <ul className="flex space-x-4">
           <li><a href="/classification">Leagues</a></li>
-          <li><a href="/participants">Participants</a></li>
+          <li><a href="/bots">Participants</a></li>
           <li><a href="/logout">Log out</a></li>
         </ul>
       </div>
