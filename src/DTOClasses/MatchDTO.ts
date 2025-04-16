57d3e126dbd75693cdedfcf1d49d5e965e9d9f62
CharlieUIB <carlos.galvez1@estudiant.uib.cat>
2025-04-16 13:23:41 +0200

    changed order of parameters

--- src/DTOClasses/MatchDTO.ts
+++ src/DTOClasses/MatchDTO.ts
@@ -1,10 +1,10 @@
 type MatchState = "PENDING" | "IN_PROCESS" | "COMPLETED"
-type MatchResult = 0 | 1 | 2 | null;
+type MatchResult = 0 | 1 | -1 | null;
 
 export interface MatchDTO {
   id: number;
   state: MatchState;
   result: MatchResult;
-  fighters: number[];
   roundNumber: number;
+  fighters: number[];
 }
